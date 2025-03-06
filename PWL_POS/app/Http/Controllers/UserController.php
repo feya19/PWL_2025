e9b5868c3214fcbbf2d952375438a07935159ef8
Fahmi Yahya <fahmi.yahyafl@gmail.com>
2025-03-06 23:12:39 +0700

    Jobsheet-4: Praktikum 1

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,23 +10,15 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
 
-        // UserModel::insert($data);
- 
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-
-        // UserModel::where('username', 'customer-1')->update($data);
-        
         $user = UserModel::all();
-        
         return view('user', ['data' => $user]);
     }
 }
