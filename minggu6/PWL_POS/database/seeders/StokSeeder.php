af51e17a88d765f5882eb3d0c8311a446b9dfd3f
Fahmi Yahya <fahmi.yahyafl@gmail.com>
2025-03-22 04:21:06 +0700

    Jobsheet-6: Praktikum 1

---
+++ minggu6/PWL_POS/database/seeders/StokSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StokSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $r_supplier = DB::table('m_supplier')->pluck('supplier_id')->toArray();
+        $r_barang = DB::table('m_barang')->pluck('barang_id')->toArray();
+        $r_user = DB::table('m_user')->pluck('user_id')->toArray();
+
+        $data = [];
+        for ($i = 0; $i < 10; $i++) {
+            $data[] = [
+                'supplier_id' => $r_supplier[array_rand($r_supplier)],
+                'barang_id' => $r_barang[array_rand($r_barang)],
+                'user_id' => $r_user[array_rand($r_user)],
+                'stok_tanggal' => now(),
+                'stok_jumlah' => rand(1, 100),
+            ];
+        }
+
+        DB::table('t_stok')->insert($data);
+    }
+}
