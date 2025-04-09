242b0f674735452576a37f8f881ac553b3445c36
Fahmi Yahya <fahmi.yahyafl@gmail.com>
2025-04-09 19:30:37 +0700

    Jobsheet-7: Praktikum 2 & Tugas 2

--- minggu7/PWL_POS/routes/web.php
+++ minggu7/PWL_POS/routes/web.php
@@ -24,12 +24,33 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu 
-    // masukkan semua route yang perlu autentikasi di sini 
+    Route::get('/', [WelcomeController::class, 'index']);
+
+    // artinya semua route di dalam group ini harus punya role ADM (Administrator)
+    Route::middleware(['authorize:ADM'])->group(function () {
+        Route::group(['prefix' => 'level'], function () {
+            Route::get('/', [LevelController::class, 'index']);
+            Route::post('/list', [LevelController::class, 'list']);
+            Route::get('/create', [LevelController::class, 'create']);
+            Route::post('/', [LevelController::class, 'store']);
+            Route::get('/create_ajax', [LevelController::class, 'create_ajax']); // Menampilkan halaman form tambah user Ajax
+            Route::post('/ajax', [LevelController::class, 'store_ajax']); // Menyimpan data user baru Ajax
+            Route::get('/{id}', [LevelController::class, 'show']);
+            Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
+            Route::get('/{id}/edit', [LevelController::class, 'edit']);
+            Route::put('/{id}', [LevelController::class, 'update']);
+            Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax
+            Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
+            Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
+            Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); // Untuk hapus data user Ajax
+            Route::delete('/{id}', [LevelController::class, 'destroy']);
+        });
+    });
 });
 // Route::get('/', function () {
 //     return view('layouts.template');
 // });
-Route::get('/', [WelcomeController::class, 'index']);
+// Route::get('/', [WelcomeController::class, 'index']);
 
 // Route::get('/level', [LevelController::class, 'index']);
 // Route::get('/kategori', [KategoriController::class, 'index']);
@@ -51,25 +72,6 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
     Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
 });
-
-Route::group(['prefix' => 'level'], function () {
-    Route::get('/', [LevelController::class, 'index']);
-    Route::post('/list', [LevelController::class, 'list']);
-    Route::get('/create', [LevelController::class, 'create']);
-    Route::post('/', [LevelController::class, 'store']);
-    Route::get('/create_ajax', [LevelController::class, 'create_ajax']); // Menampilkan halaman form tambah user Ajax
-    Route::post('/ajax', [LevelController::class, 'store_ajax']); // Menyimpan data user baru Ajax
-    Route::get('/{id}', [LevelController::class, 'show']);
-    Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
-    Route::get('/{id}/edit', [LevelController::class, 'edit']);
-    Route::put('/{id}', [LevelController::class, 'update']);
-    Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax
-    Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
-    Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
-    Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); // Untuk hapus data user Ajax
-    Route::delete('/{id}', [LevelController::class, 'destroy']);
-});
-
 
 Route::group(['prefix' => 'kategori'], function () {
     Route::get('/', [KategoriController::class, 'index']);
--- minggu7/README.md
+++ minggu7/README.md
@@ -4,15 +4,6 @@
 - **Kelas**: TI-2A
 
 ## Praktikum 1 - Implementasi Authentication
-   1. **Memodifikasi `config/auth.php`**
-
-   2. **Memodifikasi `UserModel.php`**
-
-   3. **Membuat `AuthController.php`**
-
-   4. **Membuat view `login.blade.php`**
-
-   #### Output:
    ![alt text](ss/1.1.png)
 
 ---
@@ -22,13 +13,46 @@
    ![alt text](ss/1.2.png)
 
    2. **Silahkan implementasi proses logout pada halaman web yang kalian buat**
-   ![alt text](ss/1.3.png)
+      - Menyesuaikan bagian header untuk menambahkan aksi logout.
+      ![alt text](ss/1.4.2.png)
+
+         #### Output:
+         ![alt text](ss/1.3.png)
 
    3. **Amati dan jelaskan tiap tahapan yang kalian kerjakan, dan jabarkan dalam laporan**
-   - Mengambil nama dari session login di halaman utama.
-   ![alt text](ss/1.4.png)
+      - Sesuai dengan langkah-langkah di jobsheet, yakni:
+         1. **Memodifikasi `config/auth.php`**
 
-   - Menyesuaikan bagian header untuk menambahkan aksi logout.
-   ![alt text](ss/1.4.2.png)
+         2. **Memodifikasi `UserModel.php`**
+
+         3. **Membuat `AuthController.php`**
+
+         4. **Membuat view `login.blade.php`**
+
+         5. **Menambahkan route ke `web.php`**
 
    4. **Submit kode untuk impementasi Authentication pada repository github kalian.**
+
+---
+
+## Praktikum 2 - Implementasi Authorizaton di Laravel dengan Middleware
+   - **Login Menggunakan Admin**
+   ![alt text](ss/2.1.png)
+   - **Login Menggunakan Manager**
+   ![alt text](ss/2.2.png)
+
+---
+
+## Tugas 2 - Implementasi Authorization
+   1. **Apa yang kalian pahami pada praktikum 2 ini?**
+      - Authorization atau Hak Akses setiap role dapat diatur menggunakan Middleware.
+   2. **Amati dan jelaskan tiap tahapan yang kalian kerjakan, dan jabarkan dalam laporan**
+      - Sesuai dengan langkah-langkah di jobsheet, yakni:
+      1. **Memodifikasi `UserModel.php`**
+
+      2. **Membuat `AuthorizeUser.php`**
+
+      3. **Menambahkan alias middleware di `Kernel.php`**
+
+      4. **Memodifikasi route di `web.php`**
+   3. **Submit kode untuk impementasi Authorization pada repository github kalian.**
