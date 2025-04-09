b79e896e966054125a487b4136eb5d3181f4442c
Fahmi Yahya <fahmi.yahyafl@gmail.com>
2025-04-09 21:39:51 +0700

    Jobsheet-8: Init

---
+++ minggu8/PWL_POS/resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Left navbar links -->
+  <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+          <a href="/" class="nav-link">Home</a>
+      </li>
+  </ul>
+
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+              <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="{{ url('logout') }}" role="button" style="transition: color 0.3s;"
+              onmouseover="this.style.color='red'" onmouseout="this.style.color=''">
+              {{ auth()->user()->nama }}<i class="ml-2 fas fa-sign-out-alt"></i>
+          </a>
+      </li>
+  </ul>
+</nav>
---
+++ minggu8/README.md
@@ -0,0 +1,4 @@
+# Jobsheet-8: File Import dan Export ke PDF dan Excel pada Laravel
+- **Nama**: Fahmi Yahya
+- **NIM**: 2341720089
+- **Kelas**: TI-2A
