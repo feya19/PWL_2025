85fdc6774f8a215a87a7f358c68cef6ad7010ba2
Fahmi Yahya <fahmi.yahyafl@gmail.com>
2025-04-25 20:42:16 +0700

    Jobsheet-11: Init

---
+++ minggu9/jobsheet11/PWL_POS/database/seeders/BarangSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BarangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $r_kategori = DB::table('m_kategori')->pluck('kategori_id')->toArray();
+
+        if (!empty($r_kategori)) {
+            $data = [];
+
+            for ($i = 0; $i < 10; $i++) {
+                $data[] = [
+                    'kategori_id' => $r_kategori[array_rand($r_kategori)],
+                    'barang_kode' => 'BRG' . str_pad($i + 1, 3, '0', STR_PAD_LEFT),
+                    'barang_nama' => 'Item ' . ($i + 1),
+                    'harga_beli' => rand(5000, 20000),
+                    'harga_jual' => rand(15000, 30000),
+                ];
+            }
+
+            DB::table('m_barang')->insertOrIgnore($data);
+        } else {
+            echo "No records found in m_kategori table.";
+        }
+    }
+}
---
+++ minggu9/jobsheet11/README.md
@@ -0,0 +1,4 @@
+# Jobsheet-11: RESTFUL API 2
+- **Nama**: Fahmi Yahya
+- **NIM**: 2341720089
+- **Kelas**: TI-2A
